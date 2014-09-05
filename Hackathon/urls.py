2bd1203a9e5cd050076fe390c117a090d58a8a00
Sahil Shekhawat <sahilshekhawat01@gmail.com>
2014-09-05 17:00:07 +0530

    Added landing page and login modal

--- Hackathon/urls.py
+++ Hackathon/urls.py
@@ -1,5 +1,7 @@
 from django.conf.urls import patterns, include, url
 
+from hackathon_base.views import *
+
 from django.contrib import admin
 admin.autodiscover()
 
@@ -9,4 +11,5 @@ urlpatterns = patterns('',
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^$', home),
 )
